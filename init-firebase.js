85b49ec50705818059f0196258d00f1dd59d37fa
Dheepak Ramanathan <135533984+dheepak-rmn@users.noreply.github.com>
2023-06-22 18:34:25 -0600

    Update init-firebase.js

--- init-firebase.js
+++ init-firebase.js
@@ -1,5 +1,12 @@
 // TODO: Replace the following with your app's Firebase project configuration
 // Your web app's Firebase configuration
-
+const firebaseConfig = {
+	apiKey: 'AIzaSyDsQkpI0rMD9krMACpSo7hUn4Ar7jud6Nw',
+	authDomain: 'fir-auth-dheepak-rmn.firebaseapp.com',
+	projectId: 'fir-auth-dheepak-rmn',
+	storageBucket: 'fir-auth-dheepak-rmn.appspot.com',
+	messagingSenderId: '655184856168',
+	appId: '1:655184856168:web:3c4224aa10cdc9660627dd',
+};
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
